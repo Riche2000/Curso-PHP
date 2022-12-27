922b74ba4a7599bf8a7a3b9c2281f1ef712f6cbd
riche2000 <ricardosanch2000@gmail.com>
2022-12-27 16:15:20 -0600

    Sitrings

--- strings.php
+++ strings.php
@@ -23,7 +23,17 @@ echo "<br>";
 echo strtoupper($mensaje);
 echo "<br>";
 //Comparar cadenas
-echo strcmp("hola", "adios"); //Nos arroja un valornumerico, datia 0 si fueran iguales 
+echo strcmp("hola", "adios"); //Nos arroja un valornumerico, daria 0 si fueran iguales 
 echo "<br>";
 echo strcmp("a", "b"); //Nos sale negativo porque b es más grande que b
+echo "<br>";
+echo strcmp("a", "a");
+echo "<br>";
+//Substraer cadena
+echo substr($mensaje, 10); //El string es mensaje y la posicion es 10
+echo "<br>";
+echo substr($mensaje, 10, 5); //De aqui solo substraemos 5 caracteres
+echo "<br>";
+//Remover espacios en blanco
+echo trim("    hola   soy   Ricardo"); //Solo deja un espacio
 ?>
