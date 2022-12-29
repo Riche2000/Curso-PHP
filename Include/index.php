4a004a4a8f7add7a59d317a2f3c2f0c10eb9ca07
riche2000 <ricardosanch2000@gmail.com>
2022-12-28 22:04:46 -0600

    Include y Require

--- Include/index.php
+++ Include/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Include y require</title>
+	<title>Include y Require</title>
 	<style>
 		#container{width: 500px; margin: 150px auto;}
 		#footer{background-color: #222; padding: 10px; color: white;}
@@ -15,7 +15,6 @@
 			include("menu.php"); //Se usa para llamar todo el contenido 
 		?>
 	<h3>Contenido Principal</h3>
-
 		<?php
 			require("footer.php");
 		?>
